ed7398c770ebf1c03b618731a861dee97fc4d3e8
agure-la <lameckagure@gmail.com>
2023-04-03 07:16:13 -0700

    pointers_arrays_strings completed task 1 to 8

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void *_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned int i;
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
+
+	for (i = 0; i < n; i++)
+		destination[i] = source[i];
+
+	return (dest);
+}
